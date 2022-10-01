28ae64fb27b775c533f3a0140f58b71d3fdfe92f
Max Bushuev <bushuevmaxim55@gmail.com>
2022-10-01 08:22:31 +0600

    class move

--- Program.cs
+++ Program.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Numerics;
+
 using System.Threading.Tasks;
+
+
 namespace lab1
 {
   class Program
   {
-    static void Main(string[] args)
-    {
-    }
-
+ 
     interface Movable
 {
   Vector<int> position 
@@ -37,12 +37,22 @@ namespace lab1
   
 
   }
-  void Move()
-  {
-    this.position[0] = this.position[0] + this.velocity[0];
-  }
+  
 
 }
 
+class Move
+  {
+    void move(Movable movableObject) 
+    {
+      
+        movableObject.position =  movableObject.position + movableObject.velocity;
+      
+
+    }
   }
+  }
+
+
+  
 }
