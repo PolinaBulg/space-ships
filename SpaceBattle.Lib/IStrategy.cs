b1a39c2853bcbd5db6ca7cd64425b387445546c0
Max Bushuev <bushuevmaxim55@gmail.com>
2022-12-26 19:19:22 +0600

    general interfaces

--- SpaceBattle.Lib/IStrategy.cs
+++ SpaceBattle.Lib/IStrategy.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace SpaceBattle.Lib;
 
-namespace SpaceBattle.Lib
+public interface IStrategy
 {
-    public interface IStrategy
-    {
-        
-    }
+    void Run(object [] parameters);
 }
