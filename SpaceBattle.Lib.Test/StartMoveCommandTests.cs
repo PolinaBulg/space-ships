d425e9475b4dffc3d6413f840add74d02dfa2538
Max Bushuev <bushuevmaxim55@gmail.com>
2022-12-27 19:16:11 +0600

    remove mistakes

--- SpaceBattle.Lib.Test/StartMoveCommandTests.cs
+++ SpaceBattle.Lib.Test/StartMoveCommandTests.cs
@@ -12,9 +12,9 @@ public class StartMoveCommandTests
     void InitState()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
-        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object> ("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
-        var mockCommand = new Mock<Lib.ICommand>(); 
+        var mockCommand = new Mock<Lib.ICommand>();
         var mockStrategyGetCommand = new Mock<IStrategy>();
 
         mockCommand.Setup(x => x.Execute());
@@ -26,12 +26,6 @@ public class StartMoveCommandTests
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.MoveCommand", (object[] parameters) => mockStrategyGetCommand.Object.Run(parameters)).Execute();
 
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Collections.Queue.Push", (object[] parameters) => mockStrategyGetCommand.Object.Run(parameters)).Execute();
-
-
-
-
-        
-
     }
 
     [Fact]
@@ -42,63 +36,46 @@ public class StartMoveCommandTests
         var mockTarget = new Mock<IUObject>();
         var mockMoveCommandStartable = new Mock<IMoveCommandStartable>();
 
-        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object ).Returns(mockTarget.Object).Verifiable();
-       mockMoveCommandStartable.SetupGet<IDictionary<string,object>>(x => x.Properties ).Returns(new Dictionary<string, object>{{"Velocity", new Vector(1,2)}}).Verifiable();
-        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue ).Returns(new Queue<Lib.ICommand>()).Verifiable();
+        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object).Returns(mockTarget.Object).Verifiable();
+        mockMoveCommandStartable.SetupGet<IDictionary<string, object>>(x => x.Properties).Returns(new Dictionary<string, object> { { "Velocity", new Vector(1, 2) } }).Verifiable();
+        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue).Returns(new Queue<Lib.ICommand>()).Verifiable();
 
         var startMoveCommand = new StartMoveCommand(mockMoveCommandStartable.Object);
         startMoveCommand.Execute();
 
         mockMoveCommandStartable.Verify();
-
-
-
-
-
     }
 
-   [Fact]
-    
+    [Fact]
+
     public void TryRunStartMoveComandWithoutInitialVelocity()
     {
         InitState();
         var mockTarget = new Mock<IUObject>();
         var mockMoveCommandStartable = new Mock<IMoveCommandStartable>();
 
-        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object ).Returns(mockTarget.Object).Verifiable();
-        mockMoveCommandStartable.SetupGet<IDictionary<string,object>>(x => x.Properties ).Throws(new Exception()).Verifiable();
-        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue ).Returns(new Queue<Lib.ICommand>()).Verifiable();
+        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object).Returns(mockTarget.Object).Verifiable();
+        mockMoveCommandStartable.SetupGet<IDictionary<string, object>>(x => x.Properties).Throws(new Exception()).Verifiable();
+        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue).Returns(new Queue<Lib.ICommand>()).Verifiable();
 
         var startMoveCommand = new StartMoveCommand(mockMoveCommandStartable.Object);
-        
+
         Assert.Throws<Exception>(() => startMoveCommand.Execute());
-        
-        
-
-
-
-
-    } 
+    }
 
     [Fact]
-   public void TryRunStartMoveComandWithoutTarget()
+    public void TryRunStartMoveComandWithoutTarget()
     {
         InitState();
         var mockTarget = new Mock<IUObject>();
         var mockMoveCommandStartable = new Mock<IMoveCommandStartable>();
 
-        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object ).Throws(new Exception()).Verifiable();
-        mockMoveCommandStartable.SetupGet<IDictionary<string,object>>(x => x.Properties ).Returns(new Dictionary<string, object>{{"Velocity", new Vector(1,2)}}).Verifiable();
-        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue ).Returns(new Queue<Lib.ICommand>()).Verifiable();
+        mockMoveCommandStartable.SetupGet<IUObject>(x => x.Object).Throws(new Exception()).Verifiable();
+        mockMoveCommandStartable.SetupGet<IDictionary<string, object>>(x => x.Properties).Returns(new Dictionary<string, object> { { "Velocity", new Vector(1, 2) } }).Verifiable();
+        mockMoveCommandStartable.SetupGet<IEnumerable<Lib.ICommand>>(x => x.Queue).Returns(new Queue<Lib.ICommand>()).Verifiable();
 
         var startMoveCommand = new StartMoveCommand(mockMoveCommandStartable.Object);
-        
+
         Assert.Throws<Exception>(() => startMoveCommand.Execute());
-        
-        
-
-
-
-
-    }  
+    }
 }
