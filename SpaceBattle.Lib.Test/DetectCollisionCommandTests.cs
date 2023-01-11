590ab6b47dbbce2a6e045057538ef9f36cb212ef
Max Bushuev <bushuevmaxim55@gmail.com>
2023-01-11 20:34:00 +0600

    implementation detectCommand

--- SpaceBattle.Lib.Test/DetectCollisionCommandTests.cs
+++ SpaceBattle.Lib.Test/DetectCollisionCommandTests.cs
@@ -13,18 +13,14 @@ public class DetectCollisionCommandTests
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
-        var mockStrategyGetSolutionTree = new Mock<IStrategy>();
-        mockStrategyGetSolutionTree.Setup(x => x.Run(It.IsAny<object[]>())).Returns(new Dictionary<int,object>());
         var mockStrategyCalculateDifference = new Mock<IStrategy>();
-        mockStrategyCalculateDifference.Setup(x => x.Run(It.IsAny<object[]>())).Returns(new Vector(1,4,6,6));
+        mockStrategyCalculateDifference.Setup(x => x.Run(It.IsAny<object[]>())).Returns(new Vector(It.IsAny<int>(),It.IsAny<int>(),It.IsAny<int>(),It.IsAny<int>()));
         var mockStrategyCheckCollision = new Mock<IStrategy>();
         mockStrategyCheckCollision.Setup(x => x.Run(It.IsAny<object[]>())).Returns(true);
 
-        
-
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.GetSolutionTree",(object[] parameters) => mockStrategyGetSolutionTree.Object.Run(parameters)).Execute();
+    
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.CalculateDifference", (object[] parameters) => mockStrategyCalculateDifference.Object.Run(parameters)).Execute(); 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Tools.SolutionTree.TreePass", (object[] parameters) => mockStrategyCheckCollision.Object.Run(parameters)).Execute(); 
+        
 
 
     }
@@ -34,9 +30,16 @@ public class DetectCollisionCommandTests
         var mockUObject1 = new Mock<IUObject>();
         var mockUObject2 = new Mock<IUObject>();
         
-
+        mockUObject1.SetupGet(x => x.GetProperty("Position")).Returns(new Vector(It.IsAny<int>(),It.IsAny<int>())).Verifiable();
+        mockUObject1.SetupGet(x => x.GetProperty("Velocity")).Returns(new Vector(It.IsAny<int>(),It.IsAny<int>())).Verifiable();
+        mockUObject2.SetupGet(x => x.GetProperty("Position")).Returns(new Vector(It.IsAny<int>(),It.IsAny<int>())).Verifiable();
+        mockUObject2.SetupGet(x => x.GetProperty("Velocity")).Returns(new Vector(It.IsAny<int>(),It.IsAny<int>())).Verifiable();
+        
         var detectCollisionCommand = new DetectCollisionCommand(mockUObject1.Object,mockUObject2.Object );
+        
         detectCollisionCommand.Execute();
+
+        
 
 
     }
