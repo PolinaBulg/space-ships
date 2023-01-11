af9d592a040e98432e17b02c77017e02047c0413
Max Bushuev <bushuevmaxim55@gmail.com>
2023-01-11 20:31:07 +0600

    implement detectCommand

--- SpaceBattle.Lib.Test/DetectCollisionCommandTests.cs
+++ SpaceBattle.Lib.Test/DetectCollisionCommandTests.cs
@@ -6,7 +6,7 @@ using Hwdtech.Ioc;
 using Hwdtech;
 using SpaceBattle.Lib;
 
-public class CollisionDetecterTests
+public class DetectCollisionCommandTests
 {
     void InitState()
     {
@@ -14,9 +14,17 @@ public class CollisionDetecterTests
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
         var mockStrategyGetSolutionTree = new Mock<IStrategy>();
+        mockStrategyGetSolutionTree.Setup(x => x.Run(It.IsAny<object[]>())).Returns(new Dictionary<int,object>());
+        var mockStrategyCalculateDifference = new Mock<IStrategy>();
+        mockStrategyCalculateDifference.Setup(x => x.Run(It.IsAny<object[]>())).Returns(new Vector(1,4,6,6));
+        var mockStrategyCheckCollision = new Mock<IStrategy>();
+        mockStrategyCheckCollision.Setup(x => x.Run(It.IsAny<object[]>())).Returns(true);
+
+        
 
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.GetSolutionTree",(object[] parameters) => mockStrategyGetSolutionTree.Object.Run(parameters)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.CalculateDifference"); //передать страти=егию
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Operations.CalculateDifference", (object[] parameters) => mockStrategyCalculateDifference.Object.Run(parameters)).Execute(); 
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Tools.SolutionTree.TreePass", (object[] parameters) => mockStrategyCheckCollision.Object.Run(parameters)).Execute(); 
 
 
     }
@@ -25,10 +33,11 @@ public class CollisionDetecterTests
     {
         var mockUObject1 = new Mock<IUObject>();
         var mockUObject2 = new Mock<IUObject>();
+        
 
-        var solutionTree = IoC.Resolve<IDictionary<int, object>>("Operations.GetSolutionTree");
-        var coordinatesDifference = IoC.Resolve<Vector>("Operations.CalculateDifference", mockUObject1.Object, mockUObject2.Object);
-        var detectCollisionCommand =  new DetectCollisionCommand(mockUObject1.Object, mockUObject2.Object);
+        var detectCollisionCommand = new DetectCollisionCommand(mockUObject1.Object,mockUObject2.Object );
+        detectCollisionCommand.Execute();
+
 
     }
 }
--- SpaceBattle.Lib/collision/DetectCollisionCommand.cs
+++ SpaceBattle.Lib/collision/DetectCollisionCommand.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SpaceBattle.Lib.collision
+namespace SpaceBattle.Lib;
+using Hwdtech;
+public class DetectCollisionCommand : ICommand
 {
-    public class DetectCollisionCommand
+    private IUObject firstObject, secondObject;
+    public DetectCollisionCommand(IUObject firstObject, IUObject secondObject)
     {
-        
+        this.firstObject = firstObject;
+        this.secondObject = secondObject;
+    }
+    public void Execute()
+    {
+        var coordinatesFirst = IoC.Resolve<int[]>("Operations.GetProperty", firstObject, "Position");
+        var coordinatesSecond = IoC.Resolve<int[]>("Operations.GetProperty", secondObject, "Position");
+
+        var velocityFirst = IoC.Resolve<int[]>("Operations.GetProperty", firstObject, "Velocity");
+        var velocitySecond = IoC.Resolve<int[]>("Operations.GetProperty", secondObject, "Velocity");
+
+        var vectorDelta = IoC.Resolve<int[]>("Operations.CalculateDifferences", coordinatesFirst,velocityFirst, coordinatesSecond, velocitySecond);
+        var solutionTree = IoC.Resolve<IDictionary<int, object>>("Operations.GetSolutionTree");
+
+        var result = IoC.Resolve<bool>("Tools.SolutionTree.TreePass", solutionTree, vectorDelta);
+
+        if (result)
+        {
+            throw new Exception();
+        }
+
     }
 }
